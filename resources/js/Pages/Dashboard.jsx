6c174dde6b3b4202c798b09018b3dfda5dd26ce2
ar21106 <ar21106@ues.edu.sv>
2024-07-15 21:37:04 -0600

    map station marker click handler added

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -13,7 +13,15 @@ export default function Dashboard({ auth, estacionesMap, estacion, data }) {
     //poniendo los pines en el mapa para las estaciones
     let estacionesMarker = estacionesMap.map(function (estacion) {
         return (
-            <Marker position={[estacion.latitud, estacion.longitud]}>
+            <Marker
+                position={[estacion.latitud, estacion.longitud]}
+                eventHandlers={{
+                    click: (e) => {
+                        console.log('estacion '+estacion.indice+' clickeada')
+                        //TODO (Cargar pagina con /{indice} de la estacion clickeada)
+                    }
+                }}
+            >
                 <Popup>
                     {estacion.nombre}
                 </Popup>
@@ -24,35 +32,42 @@ export default function Dashboard({ auth, estacionesMap, estacion, data }) {
     //mostrando los datos generales de la estacion elegida
     function estacionDatosGenerales(estacion) {
         if (estacion !== null) {
-            return (<>
-                <div className='text-xl font-semibold border-b-2'>
-                    Estación {estacion.indice} - {estacion.nombre}
-                </div>
-                <div>
-                    {estacion.descripcion}
-                </div>
+            return (
+                <div className='p-8 grid grid-cols-1 gap-4'>
+                    <div className='text-xl font-semibold border-b-2'>
+                        Estación {estacion.indice} - {estacion.nombre}
+                    </div>
+                    <div>
+                        {estacion.descripcion}
+                    </div>
 
-                <div className='font-semibold border-b-2'>Datos generales:</div>
-                <table className='table-auto'>
-                    <tbody>
-                        <tr>
-                            <td>Dirección</td>
-                            <td>{estacion.direccion}</td>
-                        </tr>
-                        <tr>
-                            <td>Coordenadas &#40;latitud, longitud&#41;</td>
-                            <td>
-                                &#40;{estacion.latitud}&#44; {estacion.longitud}&#41;
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </>);
+                    <div className='font-semibold border-b-2'>Datos generales:</div>
+                    <table className='table-auto'>
+                        <tbody>
+                            <tr>
+                                <td>Dirección</td>
+                                <td>{estacion.direccion}</td>
+                            </tr>
+                            <tr>
+                                <td>Coordenadas &#40;latitud, longitud&#41;</td>
+                                <td>
+                                    &#40;{estacion.latitud}&#44; {estacion.longitud}&#41;
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            );
         }
-        return (<>
-            <MapSearch></MapSearch>
-            <div>Haz click en las estaciones del mapa para ver su infomación</div>
-        </>);
+        return (
+            <div className='bg-gray-200 flex flex-col justify-center place-items-center h-96 hover:bg-gray-300'>
+                <MapSearch></MapSearch>
+                <div></div>
+                <div className='mt-8 flex'>
+                    <div className='flex-initial w-6 content-center'><InfoIcon></InfoIcon></div>
+                    <div className='flex-auto'>Haz click en las estaciones del mapa para ver su infomación</div>
+                </div>
+            </div>);
     }
 
     return (
@@ -67,13 +82,6 @@ export default function Dashboard({ auth, estacionesMap, estacion, data }) {
 
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-                        <div className='m-4'>
-                            <div className='p-2 bg-green-100 rounded-2xl text-green-900 flex'>
-                                <div className='flex-initial w-8 content-center'><InfoIcon></InfoIcon></div>
-                                <div className='flex-auto'>Haz click en las estaciones del mapa para ver su infomación</div>
-                            </div>
-                        </div>
-
                         <div className='gap-0 columns-1 md:columns-2'>
 
                             <MapContainer center={[13.8007, -88.8052]} zoom={8} scrollWheelZoom={true} className='h-96'>
@@ -84,9 +92,7 @@ export default function Dashboard({ auth, estacionesMap, estacion, data }) {
                                 {estacionesMarker}
                             </MapContainer>
 
-                            <div className='m-8 grid grid-cols-1 gap-4'>
-                                {estacionDatosGenerales(estacion)}
-                            </div>
+                            {estacionDatosGenerales(estacion)}
 
                         </div>
 
