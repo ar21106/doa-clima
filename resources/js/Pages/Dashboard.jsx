aa882d27cb64dafbaf41743ac7d690fc17b6b45e
ar21106 <ar21106@ues.edu.sv>
2024-07-19 21:11:23 -0600

    adding most recent day relevant data and graphs (temperature and humidity)

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -5,11 +5,23 @@ import { TileLayer } from 'react-leaflet/TileLayer';
 import { Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import Icon from '@mdi/react';
-import { mdiCamera, mdiChartBar, mdiChevronLeft, mdiInformationOutline, mdiMapSearchOutline, mdiOpenInNew, mdiThermometer } from '@mdi/js';
+import { mdiCamera, mdiChartBar, mdiChevronLeft, mdiInformationOutline, mdiMapSearchOutline, mdiOpenInNew, mdiWaterPercent } from '@mdi/js';
+import { mdiThermometer, mdiThermometerAlert, mdiThermometerCheck } from '@mdi/js';
 import Plot from 'react-plotly.js';
 import ImageSlider from '@/Components/ImageSlider';
 
 export default function Dashboard({ auth, estacionesMap, estacion, data, fotos }) {
+    // *** DATOS GENERALES PARA LOS GRÁFICOS ***
+    const removeButtons = ['lasso2d', 'select2d', 'resetScale2d'];
+    const config = {
+        displaylogo: false,
+        modeBarButtonsToRemove: removeButtons,
+    }
+
+    const colorHigh = 'red';
+    const colorMed = 'LimeGreen';
+    const colorLow = 'Aquamarine';
+    // *** DATOS GENERALES PARA LOS GRÁFICOS ***
 
     function mostrarDivTrigger(id) {
         var div = document.getElementById(id);
@@ -37,11 +49,14 @@ export default function Dashboard({ auth, estacionesMap, estacion, data, fotos }
     function estacionDatosGenerales(estacion) {
         if (estacion !== null) {
             var wazeLink = "https://www.waze.com/en/live-map/directions?latlng=" + estacion.latitud + "%2C" + estacion.longitud;
-            var ultimaFecha = data[0].fecha;
             var ultimoTmax = data[0].tmax;
             var ultimoTmin = data[0].tmin;
             var ultimoTs = data[0].ts;
             var ultimoHr = data[0].hr;
+
+            var f = new Date(data[0].fecha);
+            var dateFormat = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+            var ultimaFecha = f.toLocaleDateString("es", dateFormat);
 
             //indice de calor
             function heatIndex(T, R) {
@@ -56,37 +71,74 @@ export default function Dashboard({ auth, estacionesMap, estacion, data, fotos }
                 const c9 = -0.00000199;
 
                 T = (T * (9 / 5)) + 32;
-                var index = c1 + (c2 * T) + (c3 * R) + (c4 * T * R) + (c5 * (T*T)) + (c6 * (R*R)) + (c7 * (T*T) * R) + (c8 * T * (R*R)) + (c9 * (T*T) * (R*R));
+                var index = c1 + (c2 * T) + (c3 * R) + (c4 * T * R) + (c5 * (T * T)) + (c6 * (R * R)) + (c7 * (T * T) * R) + (c8 * T * (R * R)) + (c9 * (T * T) * (R * R));
                 return index;
             }
 
+            //icono temperatura dinamico
             function heatIcon(ts, hr) {
-                //TODO add icons with color acording to heat index
                 var hi = heatIndex(ts, hr);
+
                 if (hi >= 80 && hi <= 90) {
                     //caution yellow
-                    return <Icon path={mdiThermometer} size={1} color='yellow' />;
+                    return <Icon title='Temperatura: Precaución' path={mdiThermometer} size={0.8} color='gold' />;
                 } else if (hi <= 103) {
                     //extreme caution orange
+                    return <Icon title='Temperatura: Alerta media' path={mdiThermometerAlert} size={0.8} color='orange' />;
                 } else if (hi <= 124) {
                     //danger redish orange
-                } else {
+                    return <Icon title='Temperatura: Peligro' path={mdiThermometerAlert} size={0.8} color='OrangeRed' />;
+                } else if (hi > 124) {
                     //extreme danger red
+                    return <Icon title='Temperatura: Peligro Extremo' path={mdiThermometerAlert} size={0.8} color='Red' />;
                 }
                 //normal green
+                return <Icon title='Temperatura: Aceptable' path={mdiThermometerCheck} size={0.8} color='Lime' />;
+            }
+
+            //gráfico evolucion de temperatura media durante el dia
+            //TODO fix this graph it
+            function tsDia() {
+                return (
+                    <Plot
+                        className='m-2 overflow-hidden rounded-lg w-1/2 h-20 content-center'
+                        data={[
+                            {
+                                x: ['7:00 am', '2:00 pm', '9:00 pm'],
+                                y: [14.2, 19.4, 15.2],
+                                name: 'temperatura',
+                                type: 'scatter',
+                                mode: 'lines+markers',
+                            },
+                            {
+                                x: ['7:00 am', '2:00 pm', '9:00 pm'],
+                                y: [14.2, 19.4, 15.2],
+                                type: 'bar',
+                            }
+                        ]}
+                        layout={{
+                            title: 'Temperatura durante el día',
+                            autosize: true,
+                            xaxis: { title: 'Hora', size: 1},
+                            yaxis: { title: 'Temperatura °C' },
+                        }}
+                        config={{staticPlot: true}}
+                    />
+                );
             }
 
             return (
-                <div className='p-4 grid grid-cols-1 gap-4'>
+                //Nombre de la estación y descripción
+                <div className='p-4 grid grid-cols-1 gap-2'>
                     <div className='text-xl font-semibold border-b-2'>
                         Estación {estacion.indice}: {estacion.nombre}
                     </div>
-                    <div>
-                        {estacion.descripcion}
-                    </div>
 
-                    <div className='font-semibold border-b-2'>Datos generales:</div>
-                    <div className='p-2 bg-gray-100 rounded-lg text-sm'>
+                    {
+                        //Datos generales (tabla)
+                    }
+                    <div>Datos generales:</div>
+                    <div className='pl-2 pr-2 bg-gray-100 rounded-lg text-sm'>
                         <table className='table-auto'>
                             <tbody>
                                 <tr className='border-b-2'>
@@ -108,33 +160,62 @@ export default function Dashboard({ auth, estacionesMap, estacion, data, fotos }
                         </table>
                     </div>
 
-                    <div className='p-2 bg-gray-100 rounded-lg text-sm'>
-                        {heatIcon(ultimoTs, ultimoHr)}
-                        tmax: {ultimoTmax + ' C°'}
+                    {
+                        //Resumen de datos relevantes (temperatura, humedad, indice de calor)
+                    }
+                    <div className='p-1 bg-gray-100 rounded-lg text-sm flex flex-col gap-1'>
+                        <div>Datos del día {ultimaFecha + ' (últimos datos encontrados)'}</div>
+
+                        <div className='flex flex-row gap-4'>
+                            <div className='flex flex-row'>
+                                {heatIcon(ultimoTmax, ultimoHr)}
+                                max: {Math.round(ultimoTmax * 100) / 100 + ' C°'}
+                            </div>
+
+                            <div className='flex flex-row'>
+                                {heatIcon(ultimoTmin, ultimoHr)}
+                                min: {Math.round(ultimoTmin * 100) / 100 + ' C°'}
+                            </div>
+
+                            <div className='flex flex-row'>
+                                {heatIcon(ultimoTs, ultimoHr)}
+                                media: {Math.round(ultimoTs * 100) / 100 + ' C°'}
+                            </div>
+
+                            <div className='flex flex-row'>
+                                <Icon path={mdiWaterPercent} size={0.8} color='DodgerBlue' />
+                                Humedad: {ultimoHr + ' %'}
+                            </div>
+                        </div>
+                        {tsDia()}
                     </div>
 
+                    {
+                        //Botones
+                    }
                     <div className='flex flex-row justify-end gap-4'>
                         <button
-                            className='p-2 bg-blue-600 hover:bg-blue-800 text-white rounded-lg flex flex-row'
+                            className='p-2 bg-blue-600 hover:bg-blue-800 text-white rounded-lg flex flex-row text-sm'
                             onClick={() => {
                                 mostrarDiv('temperatura');
                                 document.getElementById("seccionDatos").scrollIntoView({ behavior: "smooth" })
                             }}
                         >
-                            <Icon className='mr-2 place-self-center' path={mdiChartBar} size={0.8} />
-                            Ver Datos
+                            <Icon className='mr-2 place-self-center' path={mdiChartBar} size={0.7} />
+                            Ver más Datos
                         </button>
                         <button
-                            className='p-2 bg-blue-600 hover:bg-blue-800 text-white rounded-lg flex flex-row'
+                            className='p-2 bg-blue-600 hover:bg-blue-800 text-white rounded-lg flex flex-row text-sm'
                             onClick={() => {
                                 mostrarDiv('fotos');
                                 document.getElementById("seccionFotos").scrollIntoView({ behavior: "smooth" })
                             }}
                         >
-                            <Icon className='mr-2 place-self-center' path={mdiCamera} size={0.8} />
+                            <Icon className='mr-2 place-self-center' path={mdiCamera} size={0.7} />
                             Ver Fotos
                         </button>
                     </div>
+
                 </div>
             );
         }
@@ -167,18 +248,6 @@ export default function Dashboard({ auth, estacionesMap, estacion, data, fotos }
         }
         //TODO mostrar algo cuando no haya estación seleccionada
     }
-
-    // *** DATOS GENERALES PARA LOS GRÁFICOS ***
-    const removeButtons = ['lasso2d', 'select2d', 'resetScale2d'];
-    const config = {
-        displaylogo: false,
-        modeBarButtonsToRemove: removeButtons,
-    }
-
-    const colorHigh = 'red';
-    const colorMed = 'LimeGreen';
-    const colorLow = 'Aquamarine';
-    // *** DATOS GENERALES PARA LOS GRÁFICOS ***
 
     //graficos de medicion de la temperatura en la estacion seleccionada
     function estacionTemperaturas(estacion) {
@@ -252,7 +321,7 @@ export default function Dashboard({ auth, estacionesMap, estacion, data, fotos }
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
                         <div className='gap-0 columns-1 md:columns-2'>
-                            <MapContainer center={[13.8007, -88.8052]} zoom={8} scrollWheelZoom={false} className='h-96'>
+                            <MapContainer center={[13.8007, -88.8052]} zoom={8} scrollWheelZoom={true} className='h-96'>
                                 <TileLayer
                                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
